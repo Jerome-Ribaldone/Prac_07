49c1b1b2291669826c6b6270a9cdfff90e933ad4
Jerome-Ribaldone <jerome.ribaldone@my.jcu.edu.au>
2020-10-08 16:18:40 +1000

    deliverables

---
+++ KivyDemos-master/dynamic_label.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('dynamic_label.kv')
+        return self.root
+
